603ae8995becca5de712d97656bb450c26d9ff99
ariel <ariel.berman.il@gmail.com>
2014-08-23 17:09:23 +0300

    Flask Blog 2

--- flask-blog/blog.py
+++ flask-blog/blog.py
@@ -6,6 +6,9 @@ import sqlite3
 
 #configuration
 DATABASE = 'blog.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = 'hard_to_guess'
 
 app = Flask(__name__)
 
@@ -16,14 +19,29 @@ app.config.from_object(__name__)
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
-@app.route("/")
+@app.route('/', methods=['GET', 'POST'])
 def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME'] or \
+            request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid credentials. Please try again.'
+        else:
+            session['Logged_in'] = True
+            return redirect(url_for('main'))
     return render_template('login.html')
 
-@app.route("/main")
+@app.route('/main')
 def main():
     return render_template('main.html')
 
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return  redirect(url_for('login'))
+
+
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
 
