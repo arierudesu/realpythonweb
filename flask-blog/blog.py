c39069a959cb032715f0a6af50152ce63440e1f7
ariel <ariel.berman.il@gmail.com>
2014-08-23 22:36:18 +0300

    Flask Blog 3

--- flask-blog/blog.py
+++ flask-blog/blog.py
@@ -1,10 +1,12 @@
 __author__ = 'ariel'
 
-#imports
-from flask import Flask, render_template, request, session, flash, redirect, url_for, g
+# imports
+from flask import Flask, render_template, request, session, \
+    flash, redirect, url_for, g
 import sqlite3
+from functools import wraps
 
-#configuration
+# configuration
 DATABASE = 'blog.db'
 USERNAME = 'admin'
 PASSWORD = 'admin'
@@ -12,36 +14,53 @@ SECRET_KEY = 'hard_to_guess'
 
 app = Flask(__name__)
 
-# pulls in app configuration by looking for uppercase variables
+# pulls in configurations by looking for UPPERCASE variables
 app.config.from_object(__name__)
 
-# Function used to conect to db
+
+# function used for connecting to the database
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
+
+
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME'] or \
-            request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid credentials. Please try again.'
+        if request.form['username'] != app.config['USERNAME'] or\
+                request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid Credentials. Please try again.'
         else:
-            session['Logged_in'] = True
+            session['logged_in'] = True
             return redirect(url_for('main'))
-    return render_template('login.html')
+    return render_template('login.html', error=error)
+
 
 @app.route('/main')
+@login_required
 def main():
-    return render_template('main.html')
+    g.db = connect_db()
+    cur = g.db.execute('select * from posts')
+    posts = [dict(title=row[0], post=row[1]) for row in cur.fetchall()]
+    g.db.close()
+    return render_template('main.html', posts=posts)
 
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
-    return  redirect(url_for('login'))
-
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
-
