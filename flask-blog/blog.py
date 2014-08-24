7001b741b7c650444804d633538625294cb29dab
ariel <ariel.berman.il@gmail.com>
2014-08-24 09:01:25 +0300

    Flask Blog 4 update

--- flask-blog/blog.py
+++ flask-blog/blog.py
@@ -66,7 +66,7 @@ def add():
         return  redirect(url_for('main'))
     else:
         g.db = connect_db()
-        g.db.execute('insert into posts (title, post) values (?, ?)', [request.form('title'), request.form['post']])
+        g.db.execute('insert into posts (title, post) values (?, ?)', [request.form['title'], request.form['post']])
         g.db.commit()
         g.db.close()
         flash('New entry was successfully posted')
